a7ce55496b45586d90bfaa13fa7056d4a80c10ea
paulscottrobson <paulscottrobson@gmail.com>
2020-04-01 15:25:35 +0100

    0.53 Tilemap working
    As it will.

--- basic/scripts/source/test.bas
+++ basic/scripts/source/test.bas
@@ -1,18 +1,48 @@
 cls
+palette 1,1,5
+count = 0
+start = timer()
+'
+'	Create a demo tile map
+'	
 xSize = 12
 ySize = 8
-map = alloc(xSize*ySize+4)
-
+map = alloc(xSize*ySize+5)
 !map = &ABCD
 map!1 = 16
 map!2 = xSize
 map!3 = ySize
+map!4 = 4
 for x = 0 to xSize-1
 	for y = 0 to ySize-1
-		map!(x+y*xSize+4) = (x mod 3)+x/3*256
+		map!(x+y*xSize+5) = (x mod 3)+(x+y)/3*256
 	next y
 next x
 print str$(map,16)
-tile 32,32,0,0,6,4,map
-ink 5
-frame 32,32 to 32+6*16,32+4*16
+'
+'	Blit two solid bars to the sprite layer, to cover up the tilemap overflow
+'
+block = alloc(1):!block = -1
+call drawbars(8)
+'
+'	Load images and scroll it back and forth.
+'
+sprite load "sprites.dat"
+m = 1
+repeat
+	for i = -210 to 210
+		tile 32,32,i*m,0*i*m,13,9,map
+		ink 7:frame 32,32 to 32+13*16,32+9*16
+		count = count + 1
+	next i
+	m = -m
+	cursor 12,0:print count,timer()-start
+
+until false
+'
+'	Draw bars
+'
+proc drawbars(c)
+blit 16,0,block,&0800+c,&80F0
+blit 32+13*16,0,block,&0800+c,&80F0
+endproc
--- kernel/tilemap/tilemap.asm
+++ kernel/tilemap/tilemap.asm
@@ -20,21 +20,15 @@
 ; *****************************************************************************
 
 .OSXDrawTileMap
-		push 	r0,r1,r2,r3,r4,r5,r6,r7,link
+		push 	r0,r1,r2,r3,r4,r5,r6,r7,r8,link
+		mov 	r7,#16 					; tile size
 		;
 		;		First handle where the offset is not a multiple of the tile size.
 		;		If not, shift the drawing of the first time up and/or left, and
 		;		increment the tile draw size by 1 as required.
 		;
-		; 	TODO: The above bit !
-		;
-		;		Convert the pixel offsets to tile offsets
-		;
-		ror 	r2,#4 					; this is a fudge for the above routine in 16x16
-		ror 	r3,#4 					; signed division of offset !!!
-		and 	r2,#$0FFF
-		and 	r3,#$0FFF
-		mov 	r7,#16 					; tile size
+		jsr 	#_OSXDTMProcessX
+		jsr 	#_OSXDTMProcessY
 		;
 		;		At this point we have a solid number of tiles, draw them a line at a time.
 		;	
@@ -58,18 +52,126 @@
 		jmp 	#_OSXDTM1 				; loop round again
 ;
 ._OSXDTMExit		
-		pop 	r0,r1,r2,r3,r4,r5,r6,r7,link
+		pop 	r0,r1,r2,r3,r4,r5,r6,r7,r8,link
 		ret
 
 ; *****************************************************************************
 ;
 ;		Draw tile at (R0,R1) offset in table (R2,R3), data pointer R6
+;		tile size R7.
 ;
 ; *****************************************************************************
 
 .OSXDrawOneTile
-		push 	r4,r5,link
+		push 	r0,r1,r4,r5,r8,link
+		jsr 	#OSWaitBlitter
+		stm		r0,#blitterX 			; set X and Y
+		stm 	r1,#blitterY
 
-
-		pop 	r4,r5,link
+		skp 	r2 						; if either offset -ve then background
+		jmp 	#_OSXDotBackground 		;
+		skp 	r3
+		jmp 	#_OSXDotBackground
+		mov 	r0,r2,#0 				; check offset < limits
+		ldm 	r1,r6,#2
+		sub 	r0,r1,#0
+		sklt
+		jmp 	#_OSXDotBackground
+		mov 	r0,r3,#0 				; check offset < limits
+		ldm 	r1,r6,#3
+		sub 	r0,r1,#0
+		sklt
+		jmp 	#_OSXDotBackground
+		;
+		;		On the tile map. Get the tile out and process it.
+		;
+		mov 	r0,r3,#0 				; Y offset
+		ldm 	r1,r6,#2 				; X Size
+		mult 	r0,r1,#0 				; Y * Width
+		add 	r0,r2,#4 				; add X + 4
+		add 	r0,r6,#0 				; now points into the table
+		ldm 	r0,r0,#0 				; read the tile.
+		;
+		mov 	r5,r0,#0 				; command base in R5
+		and 	r5,#$6000 				; mask out unwanted command bits.
+		add 	r5,#$0800 				; set the fill background bit.
+		mov 	r8,r0,#0 				; get colour bits out
+		ror 	r8,#8
+		and 	r8,#7
+		;
+		and 	r0,#15 					; tile number.
+		ror 	r0,#12 					; multiply by 16
+		ldm 	r1,#spriteImageMemory 	; add the image memory base
+		add 	r0,r1,#0
+		mov 	r4,r0,#0 				; put in R4
+		jmp 	#_OSXDotDraw
+		;
+		;		Come here to draw a background tile (e.g. colour 0)
+		;		
+._OSXDOTBackground		
+		mov 	r4,#_OSXTileMasks 		; R4 is the data pointer
+		mov 	r5,#$8000 				; R5 is the command base
+		ldm 	r8,r6,#4				; R8 colour
+		;
+		;		Data @ R4, Command Base @ R5 ; set up and print
+		;
+._OSXDOTDraw		
+		stm 	r4,#blitterData 		; set data
+		;
+		ldm 	r4,#colourMask 			; put colour mask into MSB
+		ror 	r4,#8
+		add 	r4,r8,#0 				; add colour in.
+		stm 	r4,#blitterCMask 		; set colour/mask.
+		;
+		add 	r5,r7,#0 				; add size of tile to command base.
+		stm 	r5,#blitterCmd 			; and draw the tile.
+		;
+		pop 	r0,r1,r4,r5,r8,link
 		ret
+
+._OSXTileMasks
+		word 	$FFFF
+
+; *****************************************************************************
+;
+;		R0 x Draw Position R2 x Offset into map in pixels, R4 count, R7 tile size
+;
+; *****************************************************************************
+
+._OSXDTMProcessX
+		mov 	r8,r2,#0 				; R8 = XO & 15
+		and 	r8,#15
+		sknz 	r8
+		jmp 	#_OSXDTMDivide
+		;
+		sub 	r0,r8,#0 				; X = X - (X & 15)
+		sub 	r2,r8,#0 				; XO = XO - (X & 15)
+		;
+		inc 	r4
+._OSXDTMDivide		
+		mov 	r8,r2,#0
+		ror 	r2,#4
+		and 	r2,#$0FFF
+		skp 	r8
+		add 	r2,#$F000
+		ret
+;
+;		Exact copy using Y
+;
+._OSXDTMProcessY
+		mov 	r8,r3,#0 				; R8 = YO & 15
+		and 	r8,#15
+		sknz 	r8
+		jmp 	#_OSXDTMDivide2
+		;
+		sub 	r1,r8,#0 				; Y = Y - (Y & 15)
+		sub 	r3,r8,#0 				; YO = YO - (Y & 15)
+		;
+		inc 	r5
+._OSXDTMDivide2
+		mov 	r8,r3,#0
+		ror 	r3,#4
+		and 	r3,#$0FFF
+		skp 	r8
+		add 	r3,#$F000
+		ret		
