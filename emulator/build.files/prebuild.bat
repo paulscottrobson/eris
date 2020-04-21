b81d0aebe77e8f5363256a2db90ee883f235193f
Paul Robson <paul@robsons.org.uk>
2020-04-21 16:02:34 +0100

    Back on Linux :)

--- emulator/build.files/prebuild.bat
+++ emulator/build.files/prebuild.bat
@@ -1,25 +1,17 @@
 @echo off
-
 pushd ..\assembler
 call build.bat
 popd 
 if errorlevel 1 goto exit
-
 pushd ..\processor
 call build.bat
 popd
 if errorlevel 1 goto exit
-
-pushd ..\code.basic
-call build.bat
-popd
-if errorlevel 1 goto exit
-
+rem python uasm.py
 pushd ..\kernel
 call build.bat
 popd
 if errorlevel 1 goto exit
-
 pushd ..\basic
 call build.bat
 popd
